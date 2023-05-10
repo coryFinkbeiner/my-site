538464ef71fe33366e0c51b22fa69f8c0d398136
steeper <portland.steeper@gmail.com>
2023-05-10 00:10:31 -0400

    adding space for sidebar with css grid

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "476ebdb76a5e2b79"; }
+/******/ 		__webpack_require__.h = function() { return "799a4b31a6098856"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,16 @@ import React from 'react'
 function Home() {
   return (
     <div className='container'>
-      <div className='center'>
-        <h1>Hello World!</h1>
+      <div className='top'>
+        <div className='left'>
+          <div>left</div>
+        </div>
+        <div className='center'>
+          <h1>Hello World!</h1>
+        </div>
       </div>
+
+
     </div>
   )
 }
