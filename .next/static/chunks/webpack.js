9154eeb1cf73605047817fd7114f341a7820054b
steeper <portland.steeper@gmail.com>
2023-05-26 00:31:50 -0400

    i am rendering the resume, but need to form it better?

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8d3f30cfff835249"; }
+/******/ 		__webpack_require__.h = function() { return "5aa3388c8f576d23"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -7,11 +7,12 @@ function Home() {
   const data = useData();
 
 
-  console.log('Home', data)
+  console.log('Home', data.resumePDF)
 
   return (
     <div>
-      Home
+      <h1>Resume</h1>
+      <iframe src={'https://docs.google.com/document/d/1tVFoqOLU90ylo5AjPYnqpv90FOukAFjQst4mnfy1o-w/edit'} title="Resume" width="100%" height="600px" />
     </div>
   );
 }
