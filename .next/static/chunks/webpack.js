94a473f18669665dcff27f061965f046d228d508
steeper <portland.steeper@gmail.com>
2023-06-06 01:29:03 -0400

    implementing the left logos

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "968896bed6f47ce2"; }
+/******/ 		__webpack_require__.h = function() { return "fd8b126180a47ea4"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/Resume.js
+++ pages/Resume.js
@@ -1,40 +1,18 @@
-import React, { useEffect, useState } from 'react';
-import { getStorage, ref, getDownloadURL } from 'firebase/storage';
+import React from 'react';
+
 
 function Resume() {
-  const [resumeURL, setResumeURL] = useState(null);
-
-  useEffect(() => {
-    const storage = getStorage();
-    const resumeRef = ref(storage, 'gs://my-site-c426f.appspot.com/Rezzzz (10).pdf');
-
-    getDownloadURL(resumeRef)
-      .then((url) => {
-        setResumeURL(url);
-      })
-      .catch((error) => {
-        console.log('Error retrieving resume URL:', error);
-      });
-  }, []);
 
   return (
+
     <div>
-      {resumeURL ? (
-        <div>
-          <style>{`
-            #sidebarToggle, .toolbarButton.pageUp, .toolbarButton.pageDown {
-              display: none !important;
-            }
-            { border: 'none' }
-          `}</style>
-          <iframe src={resumeURL} title="Resume" width="100%" height="950px" />
-        </div>
-      ) : (
-        <p>Loading resume...</p>
-      )}
+      <iframe src={'https://docs.google.com/document/d/e/2PACX-1vRQGMrs8Nbw6jLgsERQyJ9DQ-4Va5-PvGWlIVM8zi8ohzmDh0a3qtmViCnhCpvLPSznaQ_4spUQLsej/pub'} title="Resume" width="100%" height="950px" />
     </div>
+
   );
 }
 
 export default Resume;
 
+
+// fix formatting of resume
